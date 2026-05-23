49e64dcca9404c3a7b28e15dd41a0d8231b0d064
Eri <eritung0421@gmail.com>
2026-05-23 16:15:27 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const SUPABASE_URL = "https://hgoppnzgzsxhyswekfka.supabase.co"
 export const SUPABASE_ANON_KEY = "sb_publishable_QasQhg5_c6HrDgK0rzLivg_YOM1WgjG"
-export const FUNCTION_URL = `${SUPABASE_URL}/functions/v1/tarot-reading`
+export const FUNCTION_URL = `${SUPABASE_URL}/functions/v1/swift-processor`
 
